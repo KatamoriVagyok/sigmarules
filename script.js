5e58f1be819c251671c9bfaef6f8d5dd0ea9a775
Kerényi Tamás <irjdr@M2-11.mitiszk.local>
2022-10-24 09:29:33 +0200

    More shit

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const container = document.getElementById("drag-and-drop-app");
 state.x = container.offsetLeft;
 state.y = container.offsetTop;
 
+window.onload = render;
+
 function render()
 {
     const doboz = `
